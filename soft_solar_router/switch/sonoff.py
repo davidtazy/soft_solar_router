fb2a101e6c89072083a2d85b302f058fb15e33e7
davidtazy <david.allemant@gmail.com>
2023-11-13 00:48:46 +0100

    implements dry-run switch

--- soft_solar_router/switch/sonoff.py
+++ soft_solar_router/switch/sonoff.py
@@ -1,6 +1,14 @@
+import logging
+
 from soft_solar_router.application.interfaces.switch import Switch
+
+logger = logging.getLogger("sonoff")
 
 
 class SonOff(Switch):
+    state = True
+
     def set(self, state: bool) -> None:
-        pass
+        if state != self.state:
+            logger.info(f"set switch state to {state}")
+            self.state = state
